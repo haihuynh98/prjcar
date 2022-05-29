2fd7b4513f2d1dfa3710c685956fb11097166274
Hai Huynh <hai.huynh@kyanon.digital>
2022-05-29 20:20:17 +0700

    fix wordpress

--- wp-content/themes/flatsome-child/functions/tiger-start.php
+++ wp-content/themes/flatsome-child/functions/tiger-start.php
@@ -1,33 +1,4 @@
 <?php
-
-
-//if (!function_exists('tiger_preload_webfonts')) :
-//
-//    /**
-//     * Preloads the main web font to improve performance.
-//     *
-//     * Only the main web font (font-style: normal) is preloaded here since that font is always relevant (it is used
-//     * on every heading, for example). The other font is only needed if there is any applicable content in italic style,
-//     * and therefore preloading it would in most cases regress performance when that font would otherwise not be loaded
-//     * at all.
-//     *
-//     * @return void
-//     * @since Twenty Twenty-Two 1.0
-//     *
-//     */
-//    function tiger_preload_webfonts()
-//    {
-//        ?>
-<!--        <link rel="preload"-->
-<!--              href="--><?php //echo esc_url(get_theme_file_uri('assets/fonts/SourceSerif4Variable-Roman.ttf.woff2')); ?><!--"-->
-<!--              as="font" type="font/woff2" crossorigin>-->
-<!--        --><?php
-//    }
-//
-//endif;
-
-//add_action('wp_head', 'tiger_preload_webfonts');
-
 /**
  * Proper way to enqueue scripts and styles
  */
@@ -38,18 +9,18 @@ function tiger_load_scripts()
 
 add_action('wp_enqueue_scripts', 'tiger_load_scripts');
 
-function widget_theme_support()
-{
-    remove_theme_support('widgets-block-editor');
-}
-
-add_action('after_setup_theme', 'widget_theme_support');
+//function widget_theme_support()
+//{
+//    remove_theme_support('widgets-block-editor');
+//}
+//
+//add_action('after_setup_theme', 'widget_theme_support');
 
 
 // Update CSS within in Admin
-function admin_style()
-{
-    wp_enqueue_style('admin-styles', getStyleUri('admin'));
-}
-
-add_action('admin_enqueue_scripts', 'admin_style');
+//function admin_style()
+//{
+//    wp_enqueue_style('admin-styles', getStyleUri('admin'));
+//}
+//
+//add_action('admin_enqueue_scripts', 'admin_style');
